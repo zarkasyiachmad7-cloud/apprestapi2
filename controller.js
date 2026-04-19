666bd17800c910b3467231dfff87f8db4f3a046e
Achmad Zarkasyi <zarkasyiachmad7@gmail.com>
2026-04-19 17:05:17 +0700

    berhasil hapus data mahasiswa

--- controller.js
+++ controller.js
@@ -39,8 +39,7 @@ exports.tambahMahasiswa = function(req, res){
     var nama = req.body.nama;
     var jurusan = req.body.jurusan; 
 
-    connection.query(
-        "INSERT INTO mahasiswa (nim,nama,jurusan) VALUES(?,?,?)",
+    connection.query("INSERT INTO mahasiswa (nim,nama,jurusan) VALUES(?,?,?)",
         [nim, nama, jurusan],
         function(error, rows, fields){
             if(error){
@@ -49,4 +48,34 @@ exports.tambahMahasiswa = function(req, res){
                 response.ok("Berhasil menambah data !", res);
             }
     });
+}
+
+// mengubah data berdasarkan id nya 
+exports.ubahMahasiswa = function(req,res){
+    var id  = req.body.id_mahasiswa;
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+    connection.query("UPDATE mahasiswa SET nim=?,nama=?,jurusan=? WHERE id_mahasiswa=?",[nim,nama,jurusan,id],
+        function(error,rows,fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok("Berhasil ubah data mahasiswa",res);
+            }
+        }
+    )
+}
+
+// menghapus data berdarkan id
+exports.hapusMahasiswa = function(req,res){
+     var id = req.body.id_mahasiswa;
+    connection.query("DELETE FROM mahasiswa WHERE id_mahasiswa=?",[id],
+        function(error,rows,fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok("Berhasil hapus data mahasiswa",res);
+            }
+        });
 }
