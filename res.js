30d6eee97155aaa6400a1d5bf8f9ab6fc7f10800
Achmad Zarkasyi <zarkasyiachmad7@gmail.com>
2026-04-20 03:17:14 +0700

    berhasil menampilkan data group matakuliah

--- res.js
+++ res.js
@@ -8,3 +8,33 @@ exports.ok = function(values,res){
     res.json(data);
     res.end();
 }
+
+// response untuk nested mata kuliah
+exports.nestedok = function(values,res){
+    // lakukan akumulasi
+    const hasil = values.reduce((akumulasikan,item)=>{
+        // tentukan group
+        if(akumulasikan[item.nama]){
+            // buat variabel group nama mahasiswa
+            const group = akumulasikan[item.nama];
+            // cek jika isi array ada matakuliah
+            if( Array.isArray(group.matakuliah)){
+                // tambahkan value ke dalam group matakuliah
+                group.matakuliah.push(item.matakuliah);                
+            }else{
+                group.matakuliah = [group.matakuliah,item.matakuliah];
+            }
+        }else {
+            akumulasikan[item.nama]= item;
+        }
+        return akumulasikan;
+    },{})
+    
+     var data = {
+        'status' : 200,
+        'values' :hasil
+    };
+
+    res.json(data);
+    res.end;
+}
